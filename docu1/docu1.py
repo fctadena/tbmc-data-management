2006b062339a9d9321d02748a0c49cc415f1e03e
Tadena <FrancisCarlo.Tadena@ph.nestle.com>
2024-06-03 16:23:13 +0800

    Added IPYNB

--- docu1/docu1.py
+++ docu1/docu1.py
@@ -2,14 +2,27 @@ import os
 
 
 
+#docu1.py
+#PURPOSE: To save csv generated data from docusign to aws database.
+
+
 
 #CHECK IF FILE "Envelope Recipient Report.csv" EXIST
 def file_checker():
     #File "Envelope Recipient Report.csv" extracted from Docusign
+    print("Checking if 'Envelope Recipient Report.csv' exist")
     if os.path.exists("Envelope Recipient Report.csv"):
-        print("ok")
+        print("'Envelope Recipient Report.csv' exist.")
+        print("Proceeding to update database.")
+
+        #add function here..
+
+
     else:
-        print("nope")
+        print("'Envelope Recipient Report.csv' does not exist.")
+
+
+
 file_checker()
 
 
