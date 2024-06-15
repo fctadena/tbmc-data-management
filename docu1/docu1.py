52adda8da950d7df95d508b3d2db2b6749c1f205
Francis Carlo Tadena <franciscarlotadena@yahoo.com.ph>
2024-06-16 01:00:54 +0800

    Removed .env and add it on gitignore

--- docu1/docu1.py
+++ docu1/docu1.py
@@ -1,81 +1,13 @@
-# import os
-# import pandas as pd
-# import psycopg2
-# from sqlalchemy import create_engine, text, MetaData, Table
-
-# expected_columns = [
-#     '"Envelope ID"',
-#     'Subject',
-#     'Status',
-#     'Sender Name',
-#     'Recipient Name',
-#     'Routing Order',
-#     'Action',
-#     'Sent On (Date)',
-#     'Sent On (Time)',
-#     'Completed On (Date)',
-#     'Completed On (Time)',
-#     'Completion Time (DD:HH:MM)'
-# ]
-
-# db_params = {
-#     'host':'localhost',
-#     'database':'tbmc_db',
-#     'user': 'tbmc_db_user',
-#     'password': '123456',
-#     'table':'tbmc_db1'
-# }
-
-# print("Checking if 'Envelope Recipient Report.csv' exists")
-# if not os.path.exists("Envelope Recipient Report.csv"):
-#     print("File does not exist. Exiting..")
-# else:
-#     print("'Envelope Recipient Report.csv' exists. Proceeding to read file...")
-#     try:
-#         data = pd.read_csv("Envelope Recipient Report.csv")
-#         print("Successfully read file")
-#         actual_columns = [col.replace("\ufeff", "") for col in data.columns]
-#         if actual_columns == expected_columns:
-#             print("Columns are correct")
-            
-#             conn = psycopg2.connect(
-#                 host=db_params['host'],
-#                 database=db_params['database'],
-#                 user=db_params['user'],
-#                 password=db_params['password']
-#             )
-#             conn.set_session(autocommit=True)
-            
-#             engine = create_engine(f"postgresql://{db_params['user']}:{db_params['password']}@{db_params['host']}:5432/{db_params['database']}")
-            
-#             table_name = db_params['table']
-#             csv_file = "Envelope Recipient Report.csv"
-            
-#             meta = MetaData()
-#             meta.reflect(bind=engine)
-            
-#             if table_name in meta.tables:
-#                 # Drop the table
-#                 meta.tables[table_name].drop(engine, checkfirst=True)
-#                 print(f"Table '{table_name}' dropped successfully (including dependent objects).")
-#             else:
-#                 print(f"Table '{table_name}' does not exist. Proceeding to create a new table.")
-            
-#             # Upload data to database
-#             data.to_sql(table_name, engine, if_exists='replace', index=False)
-#             print(f"Data from '{csv_file}' uploaded successfully to table '{table_name}'.")
-            
-#             conn.close()
-#         else:
-#             print("Columns are not OK")
-#     except Exception as e:
-#         print(f"Error while reading file or connecting to database: {e}")
-
 
 import os
 import pandas as pd
 import psycopg2
 from sqlalchemy import create_engine, text, MetaData, Table
+from dotenv import load_dotenv, dotenv_values
+
+
+load_dotenv()
+
 
 expected_columns = [
     '"Envelope ID"',
@@ -92,13 +24,16 @@ expected_columns = [
     'Completion Time (DD:HH:MM)'
 ]
 
+
 db_params = {
-    'host': 'localhost',
-    'database': 'tbmc_db',
-    'user': 'tbmc_db_user',
-    'password': '123456',
-    'table': 'tbmc_db1'
+    'host': os.getenv('DB_HOST') or 'localhost',
+    'database': os.getenv('DB_NAME') or 'tbmc_db',
+    'user': os.getenv('DB_USER') or 'tbmc_db_user',
+    'password': os.getenv('DB_PASSWORD') or '123456',
+    'table': os.getenv('DB_TABLE') or 'tbmc_db1'
 }
+
+
 
 def check_file_and_columns(file_path):
     print(f"Checking if '{file_path}' exists")
