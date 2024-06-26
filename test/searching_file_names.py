86c1efaf6b1431bb17bdecce48569ae6d2f45e3c
Tadena <FrancisCarlo.Tadena@ph.nestle.com>
2024-06-26 15:55:57 +0800

    Worked on data

--- test/searching_file_names.py
+++ test/searching_file_names.py
@@ -3,6 +3,7 @@ import os
 def get_excel_files(folder_path):
     excel_files = []
     for file_name in os.listdir(folder_path):
+        print(file_name)
         if file_name.endswith(".xlsx") or file_name.endswith(".xls"):
             excel_files.append(file_name)
             parts = file_name.split(" - ")
@@ -15,5 +16,17 @@ def get_excel_files(folder_path):
 folder_path = "./test_data"
 excel_files = get_excel_files(folder_path)
 
-# for f in excel_files:
-#     print(f)
+
+
+def get_excel_files_to_sql(folder_path):
+    excel_files = []
+    for file_name in os.listdir(folder_path):
+        print(file_name)
+        if file_name.endswith(".xlsx") or file_name.endswith(".xls"):
+            excel_files.append(file_name)
+            parts = file_name.split(" - ")
+            if len(parts) == 2:
+                print(f"The file name is '{parts[0]}', from '{parts[1]}'.")
+            else:
+                print("Check file name. Not in format.")
+    return excel_files
