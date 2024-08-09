b7d9f351c486415b0a63142ee0141a8e1b5a7a8d
Francis Carlo Tadena <franciscarlotadena@yahoo.com.ph>
2024-08-09 22:56:29 +0800

    Get task details

--- enrollment1/enrollment1.py
+++ enrollment1/enrollment1.py
@@ -34,34 +34,66 @@ def get_workspace_gid(opts_workspaces):
         # Get multiple workspaces
         api_response = workspaces_api_instance.get_workspaces(opts_workspaces)
         for data in api_response:
-            return data['gid']
-            # pprint(data)
+            workspace_gid = int(data['gid'])
+            print(f'Workspace GID: {workspace_gid}')
+            return workspace_gid
     except ApiException as e:
         print("Exception when calling WorkspacesApi->get_workspaces: %s\n" % e)
         
         
-# workspace_gid = get_workspace_gid(opts_workspaces)
-# print(workspace_gid)
+workspace_gid = get_workspace_gid(opts_workspaces)
+
 
 
 # create an instance of the API class
 projects_api_instance = asana.ProjectsApi(api_client)
-workspace_gid = get_workspace_gid(opts_workspaces) # str | Globally unique identifier for the workspace or organization.
-opts_workspace_projects = {
-    # 'limit': 50, # int | Results per page. The number of objects to return per page. The value must be between 1 and 100.
-    # 'offset': "eyJ0eXAiOJiKV1iQLCJhbGciOiJIUzI1NiJ9", # str | Offset token. An offset to the next page returned by the API. A pagination request will return an offset token, which can be used as an input parameter to the next request. If an offset is not passed in, the API will return the first page of results. *Note: You can only pass in an offset that was returned to you via a previously paginated request.*
-    # 'archived': False, # bool | Only return projects whose `archived` field takes on the value of this parameter.
-    'opt_fields': f"https://app.asana.com/api/1.0/workspaces/{workspace_gid}/projects", # list[str] | This endpoint returns a compact resource, which excludes some properties by default. To include those optional properties, set this query parameter to a comma-separated list of the properties you wish to include.
+opts_project = {
+    'workspace': workspace_gid # str | The workspace or organization to filter projects on.
 }
 
-def get_workspace_projects(opts_workspace_projects):
-    try:
-        # Get all projects in a workspace
-        api_response = projects_api_instance.get_projects_for_workspace(workspace_gid, opts_workspace_projects)
-        for data in api_response:
-            pprint(data)
-    except ApiException as e:
-        print("Exception when calling ProjectsApi->get_projects_for_workspace: %s\n" % e)
 
-get_workspace_projects = get_workspace_projects(opts_workspace_projects)
-print(opts_workspace_projects)
+def get_project_gid(opts_project):
+    try:
+        # Get multiple projects
+        api_response = projects_api_instance.get_projects(opts_project)
+        for data in api_response:
+            project_name = data['name']
+            project_gid = data['gid']
+            
+            project_details = {
+                'project_name': project_name,
+                'project_gid': project_gid
+            }
+            
+            print(f'Project Name: {project_name}')
+            print(f'Project GID: {project_gid}')
+
+            return project_details
+            
+    except ApiException as e:
+        print("Exception when calling ProjectsApi->get_projects: %s\n" % e)
+        
+        
+project_details = get_project_gid(opts_project)
+
+
+
+
+
+
+# create an instance of the API class
+tasks_api_instance = asana.TasksApi(api_client)
+project_gid = project_details['project_gid'] # str | Globally unique identifier for the project.
+    
+    
+opts = {
+    'opt_fields': "name,created_at,completed,completed_at,due_at,f'https://app.asana.com/api/1.0/projects/{project_gid}/tasks'", # list[str] | This endpoint returns a compact resource, which excludes some properties by default. To include those optional properties, set this query parameter to a comma-separated list of the properties you wish to include.
+}
+
+try:
+    # Get tasks from a project
+    api_response = tasks_api_instance.get_tasks_for_project(project_gid, opts)
+    for data in api_response:
+        pprint(data)
+except ApiException as e:
+    print("Exception when calling TasksApi->get_tasks_for_project: %s\n" % e)
