9490ee42821a1ad74a67b32b92b790c47a8f3c66
Francis Carlo Tadena <franciscarlotadena@yahoo.com.ph>
2024-08-13 17:50:32 +0800

    Worked on duplicated function names

--- enrollment1/enrollment1.py
+++ enrollment1/enrollment1.py
@@ -4,33 +4,17 @@ from pprint import pprint
 from dotenv import load_dotenv
 import os
 import requests
-# from helpers import get_gid_from_json
+from helpers import get_gid_from_json
 import pandas as pd
 from datetime import datetime
-import json
-
-
-# def get_gid_from_json(file_path):
-#     # Open and read the JSON file
-#     with open(file_path, 'r') as file:
-#         data = json.load(file)
-    
-#     # Extract the value of gid
-#     gid = data['data'][0]['gid']
-#     return gid
-
-
-def get_gid_from_json():
-    gid = "1207998439135084"
-    return gid
 
 
 
-# gid_file_path = 'workspace.json'
+gid_file_path = 'workspace.json'
 
 load_dotenv()
 access_token = os.getenv('ASANA_TOKEN')
-workspace_gid = get_gid_from_json()
+workspace_gid = get_gid_from_json(gid_file_path)
 
 
 
@@ -128,7 +112,6 @@ def get_all_task(project_gid, opts_tasks):
         
 df = get_all_task(project_gid, opts_tasks)
 
-
 def rename_column_names(df):
     column_mapping = {
         'created_at': 'INIT DATE',
@@ -140,35 +123,19 @@ def rename_column_names(df):
     df = df.rename(columns=column_mapping)
     return df
 
-
-
 df = rename_column_names(df)
 
-
-# # Function to extract the name from the email or handle None/empty cases
-# def extract_name(assignee_dict):
-#     if assignee_dict is None:
-#         return "<unassigned>"
-    
-#     else:
-#         email = assignee_dict['name']
-#         first_name, last_name = email.split('@')[0].split('.')
-#         return f"{first_name.capitalize()} {last_name.capitalize()}"
-
-def extract_name(assignee_dict):
+# Function to extract the name from the email or handle None/empty cases
+def extract_assignee_name(assignee_dict):
     if assignee_dict is None:
-        return ""
+        return "<unassigned>"
     else:
-        email = assignee_dict.get('name')
-        if email is None:
-            return ""
-        else:
-            first_name, last_name = email.split('@')[0].split('.')
-            return f"{first_name.capitalize()} {last_name.capitalize()}"
-        
+        email = assignee_dict['name']
+        first_name, last_name = email.split('@')[0].split('.')
+        return f"{first_name.capitalize()} {last_name.capitalize()}"
 
 # Apply the function to the 'assignee' column
-df['PIC'] = df['PIC'].apply(lambda x: extract_name(x) if x else "<unassigned>")
+df['PIC'] = df['PIC'].apply(lambda x: extract_assignee_name(x) if x else "<unassigned>")
 
 def extract_date(timestamp):
     if pd.isna(timestamp) or timestamp == '':
@@ -176,36 +143,27 @@ def extract_date(timestamp):
     return timestamp.split('T')[0]
 
 # Apply the function to the 'created_at' column
-# df['created_at'] = df['created_at'].apply(lambda x: extract_date(x))
 df['INIT DATE'] = df['INIT DATE'].apply(lambda x: extract_date(x))
 
-
-def extract_name(memberships):
+def extract_membership_name(memberships):
     try:
         if memberships and isinstance(memberships, list):
-            # Check if the list has at least one dictionary
             if len(memberships) > 0 and isinstance(memberships[0], dict):
-                # Extract 'name' from the first dictionary in the list
                 return memberships[0].get('section', {}).get('name', '')
-        return ''  # Return empty string if conditions are not met
+        return ''
     except Exception as e:
         print(f"Error processing memberships: {e}")
-        return ''  # Return empty string in case of any exception
+        return ''
 
 # Apply the function to the 'memberships' column
-df['STATUS'] = df['STATUS'].apply(lambda x: extract_name(x))
-
+df['STATUS'] = df['STATUS'].apply(lambda x: extract_membership_name(x))
 
 def extract_names(tag_list):
-    if not tag_list:  # Check if the record is empty or None
+    if not tag_list:
         return []
-    return [tag['name'] for tag in tag_list]  # Extract the 'name' values
+    return [tag['name'] for tag in tag_list]
 
 df['tags'] = df['tags'].apply(lambda x: extract_names(x))
-
-
-
-
 
 def determine_project_type(tags):
     if 'OLD' in tags:
@@ -217,7 +175,6 @@ def determine_project_type(tags):
 
 df['PROJECT_TYPE'] = df['tags'].apply(lambda x: determine_project_type(x))
 
-
 def determine_area_type(tags):
     if 'COFFEE' in tags:
         return 'COFFEE'
@@ -228,9 +185,7 @@ def determine_area_type(tags):
 
 df['AREA'] = df['tags'].apply(lambda x: determine_area_type(x))
 
-
-
-def determine_type(tags):
+def determine_task_type(tags):
     if 'TARGET' in tags:
         return 'TARGET'
     elif 'ADDITIONAL' in tags:
@@ -238,7 +193,7 @@ def determine_type(tags):
     else:
         return '<no_type>'
 
-df['TYPE'] = df['tags'].apply(lambda x: determine_area_type(x))
+df['TYPE'] = df['tags'].apply(lambda x: determine_task_type(x))
 
 def determine_doc_type(tags):
     if 'MPLAN' in tags:
@@ -249,20 +204,6 @@ def determine_doc_type(tags):
         return '<no_doc_type>'
 
 df['DOC_TYPE'] = df['tags'].apply(lambda x: determine_doc_type(x))
-
-
-def determine_type(tags):
-    if 'TARGET' in tags:
-        return 'TARGET'
-    elif 'ADDITIONAL' in tags:
-        return 'ADDITIONAL'
-    else:
-        return '<no_type>'
-
-df['TYPE'] = df['tags'].apply(lambda x: determine_area_type(x))
-
-
-
 
 project_list = [
     "AUTOCOMPACTOR",
@@ -280,36 +221,25 @@ project_list = [
     "PEC 2020: Spot Cooling"
 ]
 
-
-
-# Function to find the project in tags
 def find_project(tags):
     for project in project_list:
         if project in tags:
             return project
-    return None  # or return an empty string "" if no match is found
+    return None
 
-# Apply the function to the tags column using lambda
 df['PROJECT'] = df['tags'].apply(lambda x: find_project(x))
 
-
-
 def calculate_age(init_date_str):
-    # Convert the string date to a datetime object
     init_date = pd.to_datetime(init_date_str)
-    # Get today's date
     today = datetime.now()
-    # Calculate the difference in days
     age_days = (today - init_date).days
     return age_days
 
 df['INIT_AGE (DAYS)'] = df['INIT DATE'].apply(lambda x: calculate_age(x))
 
-
 def task_stories(row):
     return "Temporary REMARKS"
 
 df['REMARKS'] = df.apply(lambda row: task_stories(row), axis=1)
-
 
 print(df)
